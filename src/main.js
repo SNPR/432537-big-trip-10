f3b542a3c080c59d7d7c8185dd206ef9b5823f82
snpr <vaugk13@gmail.com>
2019-11-19 22:14:18 +0500

    Add trip info and render function

---
+++ src/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const getTripInfo = () => {
+  const tripInfo = `
+    <div class="trip-info__main">
+      <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
+      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
+    </div>
+  `;
+
+  return tripInfo;
+};
+
+const renderElement = (container, element) => {
+  container.insertAdjacentHTML(`beforeend`, element);
+};
