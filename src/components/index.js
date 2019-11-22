1c58416d1bb99cf29815b25a6b381d1c9d6610ed
snpr <vaugk13@gmail.com>
2019-11-22 13:47:10 +0500

    Split code to the components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import {getEventsContent} from "./events-content";
+import {getEventsSorting} from "./events-sorting";
+import {getFilters} from "./filters";
+import {getMenu} from "./menu";
+import {getTripDayItem} from "./trip-day-item";
+import {getTripDays} from "./trip-days";
+import {getTripInfo} from "./trip-info";
+
+export {
+  getEventsContent,
+  getEventsSorting,
+  getFilters,
+  getMenu,
+  getTripDayItem,
+  getTripDays,
+  getTripInfo
+};
