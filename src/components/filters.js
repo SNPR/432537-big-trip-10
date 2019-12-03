d1b2d7784906d26b88576fd7eb8e6cddf459ccf7
snpr <vaugk13@gmail.com>
2019-12-03 13:26:23 +0500

    Fix alignment

--- src/components/filters.js
+++ src/components/filters.js
@@ -4,19 +4,20 @@ export const getFilters = (filters) => {
     ${filters
       .map((filter) => {
         return `
-      <div class="trip-filters__filter">
-      <input
-        id="filter-${filter.name}"
-        class="trip-filters__filter-input  visually-hidden"
-        type="radio"
-        name="trip-filter"
-        value="${filter.name}"
-        ${filter.checked && `checked`}
-      />
-      <label class="trip-filters__filter-label" for="filter-${filter.name}"
-        >${filter.name}</label
-      >
-    </div>
+          <div class="trip-filters__filter">
+            <input
+              id="filter-${filter.name}"
+              class="trip-filters__filter-input  visually-hidden"
+              type="radio"
+              name="trip-filter"
+              value="${filter.name}"
+              ${filter.checked && `checked`}
+            />
+            <label class="trip-filters__filter-label" for="filter-
+            ${filter.name}">
+            ${filter.name}
+            </label>
+          </div>
       `;
       })
       .join(``)}
