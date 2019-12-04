181536647ae14d4dfece716a0bc35c6eddc93338
snpr <vaugk13@gmail.com>
2019-12-04 11:15:24 +0500

    Add createElement function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
 export const renderElement = (container, element, position = `beforeend`) => {
   container.insertAdjacentHTML(position, element);
 };
+
+export const createElement = (template) => {
+  const element = document.createElement(`div`);
+  element.innerHTML = template;
+
+  return template.firstChild;
+};
