767fd784a545226139c931c5d8a350a0b2c08d97
snpr <vaugk13@gmail.com>
2019-12-04 11:24:20 +0500

    Fix createElement funciton

--- src/utils.js
+++ src/utils.js
@@ -6,5 +6,5 @@ export const createElement = (template) => {
   const element = document.createElement(`div`);
   element.innerHTML = template;
 
-  return template.firstChild;
+  return element.firstChild;
 };
