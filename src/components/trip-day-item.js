ed4944bcb9c390bcecafc729aa4c831dcaeb5de1
snpr <vaugk13@gmail.com>
2019-12-08 21:44:20 +0500

    Don't show current day and date if sorting is not default

--- src/components/trip-day-item.js
+++ src/components/trip-day-item.js
@@ -9,13 +9,16 @@ export default class TripDayItem extends AbstractComponent {
 
   getTemplate() {
     return `<li class="trip-days__item  day">
-      <div class="day__info">
-        <span class="day__counter">${this._day}</span>
-        <time class="day__date" datetime="${this._date}">
-        ${new Date(this._date).toLocaleString(`en-US`, {month: `short`})}
-        ${new Date(this._date).getDate()}
-        </time>
-      </div>
+        <div class="day__info">
+          <span class="day__counter">${this._day || ``}</span>
+          <time class="day__date" datetime="${this._date || ``}">
+          ${(this._date &&
+            new Date(this._date).toLocaleString(`en-US`, {month: `short`})) ||
+            ``}
+          ${new Date(this._date).getDate() || ``}
+          </time>
+        </div>
+
       <ul class="trip-events__list">
       </ul>
     </li>`;
