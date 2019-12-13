48f25a2244bc50b2f7bfadbb8cb2f7d6828ee357
snpr <vaugk13@gmail.com>
2019-12-13 16:34:10 +0500

    Rename getDuration -> getTripDuration

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,7 +2,7 @@ import moment from "moment";
 
 export const parseTime = (UTCTimestamp) => moment(UTCTimestamp).format(`hh:mm`);
 
-export const getDuration = (startDateUTCTimestamp, endDateUTCTimestamp) => {
+export const getTripDuration = (startDateUTCTimestamp, endDateUTCTimestamp) => {
   const monthName = moment(startDateUTCTimestamp).format(`MMM`);
   const startDay = moment(startDateUTCTimestamp).format(`DD`);
   const endDay = moment(endDateUTCTimestamp).format(`DD`);
