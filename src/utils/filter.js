603381b4f00ddd32c72841845fff521c4453bdbc
snpr <vaugk13@gmail.com>
2019-12-18 14:17:33 +0500

    Add filter utils

---
+++ src/utils/filter.js
@@ -0,0 +1,23 @@
+import {FilterType} from './constants';
+
+
+export const getFuturePoints = (points) => {
+  return points.filter((point) => point.startDate > Date.now());
+};
+
+export const getPastPoints = (points) => {
+  return points.filter((point) => point.endDate < Date.now());
+};
+
+export const getPointsByFilter = (points, filterType) => {
+  switch (filterType) {
+    case FilterType.EVERYTHING:
+      return points;
+    case FilterType.FUTURE:
+      return getFuturePoints(points);
+    case FilterType.PAST:
+      return getPastPoints(points);
+  }
+
+  return points;
+};
