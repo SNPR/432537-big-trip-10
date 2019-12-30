46db396ce8c8f6e3391c9fb5119f714320301605
snpr <vaugk13@gmail.com>
2019-12-30 14:30:24 +0500

    Create Point model

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import {Method} from "./utils/constants";
-import Points from "./models/point";
+import Point from "./models/point";
 
 const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
---
+++ src/models/point.js
@@ -0,0 +1,43 @@
+export default class Point {
+  constructor(data) {
+    this.type = data[`type`];
+    this.city = data[`destination`];
+    this.startDate = data[`date_from`];
+    this.endDate = data[`date_to`];
+    this.offers = data[`offers`];
+    this.photos = data[`destination`][`pictures`];
+    this.description = data[`destination`][`description`];
+    this.price = data[`base_price`];
+    this.isFavorite = data[`is_favorite`];
+    this.id = data[`id`];
+  }
+
+  toRAW() {
+    return {
+      base_price: this.price,
+      date_from: this.startDate,
+      date_to: this.endDate,
+      destination: {
+        description: this.description,
+        name: this.city,
+        pictures: this.pictures
+      },
+      id: this.id,
+      is_favorite: this.isFavorite,
+      offers: this.offers,
+      type: this.type
+    };
+  }
+
+  static parsePoint(data) {
+    return new Point(data);
+  }
+
+  static parsePoints(data) {
+    return data.map(Point.parsePoint);
+  }
+
+  static clone(data) {
+    return new Point(data.toRAW());
+  }
+}
