ee00c2142b6e696031d280719d4a39dfd94ce895
snpr <vaugk13@gmail.com>
2019-12-31 20:26:17 +0500

    Use title for the offer name in the Card component

--- src/components/card.js
+++ src/components/card.js
@@ -42,7 +42,7 @@ export default class Card extends AbstractComponent {
         .map((offer) => {
           return `
             <li class="event__offer">
-              <span class="event__offer-title">${offer.name}</span>
+              <span class="event__offer-title">${offer.title}</span>
               &plus; &euro;&nbsp;
               <span class="event__offer-price">${offer.price}</span>
             </li>
