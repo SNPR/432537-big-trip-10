da11f0027d160005bf8f59b8d75061f84c7e9988
snpr <vaugk13@gmail.com>
2020-01-06 13:10:37 +0500

    Save static files to cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,32 @@
-self.addEventListener(`install`, (evt) => {});
+const CACHE_PREFIX = `big_trip-cache`;
+const CACHE_VER = `v1`;
+const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
+
+self.addEventListener(`install`, (evt) => {
+  evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll([
+          `/`,
+          `/index.html`,
+          `/bundle.js`,
+          `/css/style.css`,
+          `/img/icons/bus.png`,
+          `/img/icons/check-in.png`,
+          `/img/icons/drive.png`,
+          `/img/icons/flight.png`,
+          `/img/icons/restaurant.png`,
+          `/img/icons/ship.png`,
+          `/img/icons/sightseeing.png`,
+          `/img/icons/taxi.png`,
+          `/img/icons/train.png`,
+          `/img/icons/transport.png`,
+          `/img/header-bg.png`,
+          `/img/header-bg@2x.png`,
+          `/img/logo.png`
+        ]);
+      })
+  );
+});
 
 self.addEventListener(`activate`, (evt) => {});
 
