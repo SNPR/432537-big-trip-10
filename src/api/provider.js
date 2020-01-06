3d2cfb4fb2191ba7f416ab44586539ee6496582b
snpr <vaugk13@gmail.com>
2020-01-06 23:50:48 +0500

    Add CRUD operations for the points in the local storage

--- src/api/provider.js
+++ src/api/provider.js
@@ -1,5 +1,6 @@
 import nanoid from "nanoid";
 import Point from "../models/point";
+import Store from "../store";
 
 export default class Provider {
   constructor(api, backup) {
@@ -9,53 +10,97 @@ export default class Provider {
 
   getPoints() {
     if (this._isOnLine()) {
-      return this._api.getPoints();
+      return this._api.getPoints().then((points) => {
+        const rawPoints = points.map((point) => point.toRAW());
+        this._backup.setItem(`points`, rawPoints);
+
+        return points;
+      });
     }
 
-    return Promise.resolve(Point.parsePoints([]));
+    return Promise.resolve(Point.parsePoints(this._backup.getAll().points));
   }
 
   getDestinations() {
     if (this._isOnLine()) {
-      return this._api.getDestinations();
+      return this._api.getDestinations().then(() => {
+        this._backup.setItem(`destinations`, Store.getDestinations());
+      });
     }
 
-    return Promise.resolve([]);
+    return Store.setDestinations(this._backup.getAll().destinations);
   }
 
   getOffers() {
     if (this._isOnLine()) {
-      return this._api.getOffers();
+      return this._api.getOffers().then(() => {
+        this._backup.setItem(`offers`, Store.getOffers());
+      });
     }
 
-    return Promise.resolve([]);
+    return Store.setOffers(this._backup.getAll().offers);
   }
 
   createPoint(point) {
     if (this._isOnLine()) {
-      return this._api.createPoint(point);
+      return this._api.createPoint(point).then((newPoint) => {
+        this._backup.setItem(`points`, [
+          ...this._backup.getAll().points,
+          newPoint.toRAW()
+        ]);
+        return newPoint;
+      });
     }
 
     const fakeNewPointId = nanoid();
+
     const fakeNewPoint = Point.parsePoint(
         Object.assign({}, point.toRAW(), {id: fakeNewPointId})
     );
+
+    this._backup.setItem(`points`, [
+      ...this._backup.getAll().points,
+      Object.assign({}, fakeNewPoint.toRAW(), {offline: true})
+    ]);
 
     return Promise.resolve(fakeNewPoint);
   }
 
   updatePoint(id, data) {
     if (this._isOnLine()) {
-      return this._api.updatePoint(id, data);
+      return this._api.updatePoint(id, data).then((newPoint) => {
+        this._backup.setItem(`points`, [
+          ...this._backup.getAll().points,
+          newPoint.toRAW()
+        ]);
+        return newPoint;
+      });
     }
 
-    return Promise.resolve(data);
+    const fakeUpdatedPoint = Point.parsePoint(
+        Object.assign({}, data.toRAW(), {id})
+    );
+
+    this._backup.setItem(`points`, [
+      ...this._backup.getAll().points.filter((point) => point.id !== id),
+      Object.assign({}, fakeUpdatedPoint.toRAW(), {offline: true})
+    ]);
+
+    return Promise.resolve(fakeUpdatedPoint);
   }
 
   deletePoint(id) {
     if (this._isOnLine()) {
-      return this._api.deletePoint(id);
+      return this._api.deletePoint(id).then(() => {
+        this._backup.setItem(`points`, [
+          ...this._backup.getAll().points.filter((point) => point.id !== id)
+        ]);
+      });
     }
+
+    this._backup.setItem(`points`, [
+      ...this._backup.getAll().points.filter((point) => point.id !== id)
+    ]);
 
     return Promise.resolve();
   }
