928e910e7ce3668098dec819b6f3bee4949e0515
snpr <vaugk13@gmail.com>
2020-01-14 17:33:10 +0500

    Create AppController class

--- src/main.js
+++ src/main.js
@@ -1,18 +1,4 @@
-import {MenuComponent, StatisticsComponent} from "./components";
-import TripController from "./controllers/trip";
-import {renderElement, RenderPosition} from "./utils/render";
-import PointsModel from "./models/points";
-import FilterController from "./controllers/filter.js";
-import {
-  AUTHORIZATION,
-  END_POINT,
-  MenuItem,
-  menuItems,
-  BACKUP_NAME
-} from "./utils/constants";
-import API from "./api/index";
-import Backup from "./api/backup.js";
-import Provider from "./api/provider.js";
+import AppController from "./controllers/app";
 
 window.addEventListener(`load`, () => {
   navigator.serviceWorker
@@ -21,72 +7,4 @@ window.addEventListener(`load`, () => {
     .catch(() => {});
 });
 
-const tripControls = document.querySelector(`.trip-main__trip-controls`);
-const tripEvents = document.querySelector(`.trip-events`);
-const siteMainElement = document.querySelector(`.page-body__page-main`);
-
-const api = new API(END_POINT, AUTHORIZATION);
-const backup = new Backup(BACKUP_NAME, window.localStorage);
-const apiWithProvider = new Provider(api, backup);
-
-const menuComponent = new MenuComponent(menuItems);
-const pointsModel = new PointsModel();
-
-const statisticsComponent = new StatisticsComponent(pointsModel);
-const filterController = new FilterController(tripControls, pointsModel);
-const tripController = new TripController(
-    tripEvents,
-    pointsModel,
-    apiWithProvider,
-    filterController
-);
-
-Promise.all([
-  apiWithProvider.getDestinations(),
-  apiWithProvider.getOffers(),
-  apiWithProvider.getPoints()
-]).then((values) => {
-  pointsModel.setPoints(values[2]);
-  tripController.render(values[2]);
-  filterController.render();
-});
-
-renderElement(tripControls, menuComponent, RenderPosition.BEFOREEND);
-
-document
-  .querySelector(`.trip-main__event-add-btn`)
-  .addEventListener(`click`, () => {
-    tripController.createPoint();
-  });
-
-renderElement(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
-statisticsComponent.hide();
-
-menuComponent.setChangeHandler((menuItem) => {
-  switch (menuItem) {
-    case MenuItem.TABLE:
-      menuComponent.setActiveItem(MenuItem.TABLE);
-      tripController.show();
-      statisticsComponent.hide();
-      break;
-    case MenuItem.STATS:
-      menuComponent.setActiveItem(MenuItem.STATS);
-      statisticsComponent.show();
-      tripController.hide();
-      break;
-  }
-});
-
-window.addEventListener(`online`, () => {
-  document.title = document.title.replace(` [offline]`, ``);
-
-  if (!apiWithProvider.getSynchronize()) {
-    apiWithProvider.sync().catch((err) => {
-      throw new Error(err);
-    });
-  }
-});
-
-window.addEventListener(`offline`, () => {
-  document.title += ` [offline]`;
-});
+new AppController().init();
